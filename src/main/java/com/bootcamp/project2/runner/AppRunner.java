155716f7c9e19bd2bdc2692b6436bd0a0cf529dd
Thisseas X <thisseasx@yahoo.com>
2019-07-22 22:48:14 +0300

    Reduce AppRunner complexity

--- src/main/java/com/bootcamp/project2/runner/AppRunner.java
+++ src/main/java/com/bootcamp/project2/runner/AppRunner.java
@@ -1,23 +1,62 @@
 package com.bootcamp.project2.runner;
 
+import com.bootcamp.project2.service.AssignmentService;
+import com.bootcamp.project2.service.CourseService;
+import com.bootcamp.project2.service.StudentService;
+import com.bootcamp.project2.service.TrainerService;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-public interface AppRunner extends CommandLineRunner {
-    void printAllStudents();
+import java.util.Scanner;
 
-    void printAllTrainers();
+@AllArgsConstructor
+@Component
+public class AppRunner implements CommandLineRunner {
 
-    void printAllAssignments();
+    private StudentService studentService;
+    private CourseService courseService;
+    private AssignmentService assignmentService;
+    private TrainerService trainerService;
 
-    void printAllCourses();
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        loop:
+        while (true) {
+            String action = sc.next();
 
-    void printAllStudentsPerCourse();
-
-    void printAllTrainersPerCourse();
-
-    void printAllAssignmentsPerCourse();
-
-    void printAllAssignmentsPerCoursePerStudent();
-
-    void printAllStudentsWithMoreThanOneCourse();
+            switch (action) {
+                case "1":
+                    studentService.printAllStudents();
+                    break;
+                case "2":
+                    trainerService.printAllTrainers();
+                    break;
+                case "3":
+                    assignmentService.printAllAssignments();
+                    break;
+                case "4":
+                    courseService.printAllCourses();
+                    break;
+                case "5":
+                    courseService.printAllStudentsPerCourse();
+                    break;
+                case "6":
+                    courseService.printAllTrainersPerCourse();
+                    break;
+                case "7":
+                    courseService.printAllAssignmentsPerCourse();
+                    break;
+                case "8":
+                    studentService.printAllAssignmentsPerCoursePerStudent();
+                    break;
+                case "9":
+                    studentService.printAllStudentsWithMoreThanOneCourse();
+                    break;
+                case "exit":
+                    break loop;
+            }
+        }
+    }
 }
