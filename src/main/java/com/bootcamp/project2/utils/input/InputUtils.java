ecaff4158565476af1c3e27368d93b8fabac50c2
Thisseas X <thisseasx@yahoo.com>
2019-09-01 00:59:50 +0300

    Add documentation to InputUtils

--- src/main/java/com/bootcamp/project2/utils/input/InputUtils.java
+++ src/main/java/com/bootcamp/project2/utils/input/InputUtils.java
@@ -12,6 +12,15 @@ import java.util.function.Consumer;
 
 public class InputUtils {
 
+    /**
+     * Reflectively analyze the given class and map the user input
+     * to the entity's fields marked with {@link Column} annotation.
+     *
+     * @param clazz The desired class to be created and mapped.
+     * @param <T> An entity with String-type @Column-annotated fields.
+     * @return Returns a filled entity of the given class.
+     * @throws MappingException Thrown in case of a reflection error.
+     */
     public static <T> T mapInputToEntity(Class<T> clazz) throws MappingException {
         try {
             Scanner sc = new Scanner(System.in);
@@ -33,6 +42,13 @@ public class InputUtils {
         }
     }
 
+    /**
+     * Ask the user to choose an existing entity.
+     * @param repository The repository used to fetch the entity.
+     * @param prompt The question to be printed.
+     * @param <T> An entity to be chosen.
+     * @return Returns the chosen entity.
+     */
     public static <T> T requestEntityChoice(JpaRepository<T, Integer> repository, String prompt) {
         Scanner sc = new Scanner(System.in);
         IndexedList<T> list = new IndexedList<>(repository.findAll());
@@ -49,10 +65,23 @@ public class InputUtils {
         }
     }
 
+    /**
+     * Overload of {@link InputUtils#requestInputAndPersist(Class, JpaRepository, Consumer)} without a mutator.
+     */
     public static <T> void requestInputAndPersist(Class<T> clazz, JpaRepository<T, Integer> repository) {
         requestInputAndPersist(clazz, repository, null);
     }
 
+    /**
+     * Uses the above methods to create a new entity filled with the user's input and
+     * save it to the database using the given repository. Optionally accepts a mutator,
+     * used to give the ability to manipulate the entity before it is saved, e.g. filling
+     * non-Column fields with other means, such as a separate call to {@link InputUtils#requestEntityChoice(JpaRepository, String)}.
+     * @param clazz The desired class to be created and saved.
+     * @param repository The repository used to save the entity.
+     * @param mutator (Optional) A {@link Consumer} that can be used to fill the entity's fields through other means.
+     * @param <T> An entity to be created and saved.
+     */
     public static <T> void requestInputAndPersist(Class<T> clazz, JpaRepository<T, Integer> repository, Consumer<T> mutator) {
         try {
             T entity = mapInputToEntity(clazz);
@@ -60,7 +89,7 @@ public class InputUtils {
                 mutator.accept(entity);
             }
             repository.save(entity);
-            System.out.printf("-- %s created successfully! --%n", clazz.getSimpleName());
+            System.out.printf("-- %s: [%s] was successfully saved --%n", clazz.getSimpleName(), entity);
         } catch (MappingException e) {
             System.out.printf("-- Failed to create %s --%n", clazz.getSimpleName());
         }
